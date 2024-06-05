12d57618f489e028e1accda39272b69b55baec0c
nirual <laurinschweers@gmail.com>
2024-06-05 22:20:07 +0200

    rip1

--- src/Uebung5/Stapel.java
+++
@@ -1,9 +0,0 @@
-package Uebung5;
-
-public interface Stapel<T> {
-    public void pop();
-    public T top();
-    public void pushElement(T e);
-    void push(Object object);
-    public boolean isEmpty();
-}
